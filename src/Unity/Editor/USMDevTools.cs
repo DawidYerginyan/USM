833ef48bd01fcb1572673775f140e8280c524ae4
Dawid Yerginyan <dawid.yerginyan@gmail.com>
2019-05-21 13:10:18 +0200

    feat(middleware): dev tools

--- src/Unity/Editor/USMDevTools.cs
+++ src/Unity/Editor/USMDevTools.cs
@@ -1,11 +1,27 @@
-using UnityEngine;
 using UnityEditor;
 
-public class USMDevTools : EditorWindow
+using USM.Middleware;
+
+namespace USM.Unity
 {
-  [MenuItem("Window/USM Developer Tools")]
-  public static void open()
+  public partial class USMDevTools<State> : EditorWindow
   {
-    GetWindow<USMDevTools>("Unity State Manager Developer Tools");
+    private Store<State> store;
+
+    private MiddlewareChainer chainer = next => action => {
+      next(action);
+    };
+
+    public USMDevTools<State> applyStore(Store<State> store)
+    {
+      this.store = store;
+
+      return this;
+    }
+
+    public MiddlewareChainer registerChainer()
+    {
+      return this.chainer;
+    }
   }
 }
