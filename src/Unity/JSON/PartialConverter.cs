5fa2385de5a5912851fea5edf3eeaa7f819f9623
Dawid Yerginyan <dawid.yerginyan@gmail.com>
2019-05-22 13:24:04 +0200

    feat(Json.NET): JsonConverter for Vector3 Unity struct type
     • Generic PartialConverter<T> for transforming any type to desired json serialization format

---
+++ src/Unity/JSON/PartialConverter.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace USM.Unity.Json
+{
+  public abstract class PartialConverter<T> : JsonConverter
+  {
+    private static MemberInfo GetMember(string name)
+    {
+      BindingFlags flag = BindingFlags.Instance | BindingFlags.Public;
+      FieldInfo field = typeof(T).GetField(name, flag);
+
+      if (field != null)
+      {
+        return field;
+      }
+
+      PropertyInfo property = typeof(T).GetProperty(name, flag);
+
+      if (property == null)
+      {
+        Throw(name, "Public instance field or property {0} not found.");
+      }
+
+      if (property.GetGetMethod() == null)
+      {
+        Throw(name, "Property getter {0} is not readable.");
+      }
+
+      if (property.GetSetMethod() == null)
+      {
+        Throw(name, "Property setter {0} is not writable.");
+      }
+
+      if (property.GetIndexParameters().Any())
+      {
+        Throw(name, "Not support property {0} with indexes.");
+      }
+
+      return property;
+    }
+
+    private static void Throw(string name, string format)
+    {
+      throw new ArgumentException(
+        string.Format(format, $"{typeof(T).Name}.{name}"),
+        "name"
+      );
+    }
+
+    private static object GetValue(MemberInfo member, object target)
+    {
+      if (member is FieldInfo)
+      {
+        return (member as FieldInfo).GetValue(target);
+      }
+
+      return (member as PropertyInfo).GetValue(target, null);
+    }
+
+    private static void SetValue(MemberInfo member, object target, object value)
+    {
+      if (member is FieldInfo)
+      {
+        (member as FieldInfo).SetValue(target, value);
+        return;
+      }
+
+      (member as PropertyInfo).SetValue(target, value, null);
+    }
+
+    private static Type GetValueType(MemberInfo member)
+    {
+      if (member is FieldInfo)
+      {
+        return (member as FieldInfo).FieldType;
+      }
+
+      return (member as PropertyInfo).PropertyType;
+    }
+
+    private static Dictionary<string, MemberInfo> properties;
+
+    private Dictionary<string, MemberInfo> GetProperties()
+    {
+      if (null != properties)
+      {
+        return properties;
+      }
+
+      string[] names = GetPropertyNames();
+
+      if (names == null || !names.Any())
+      {
+        throw new InvalidProgramException("GetPropertyNames() cannot return empty.");
+      }
+
+      if (names.Any(name => string.IsNullOrEmpty(name)))
+      {
+        throw new InvalidProgramException("GetPropertyNames() cannot contain empty value.");
+      }
+
+      properties = names.Distinct()
+        .ToDictionary(
+          name => name,
+          name => GetMember(name)
+        );
+
+      return properties;
+    }
+
+    protected abstract string[] GetPropertyNames();
+
+    protected virtual T CreateInstance()
+    {
+      return Activator.CreateInstance<T>();
+    }
+
+    public override bool CanConvert(Type objectType)
+    {
+      return typeof(T) == objectType;
+    }
+
+    public override object ReadJson(
+      JsonReader reader,
+      Type objectType,
+      object existingValue,
+      JsonSerializer serializer
+    )
+    {
+      if (reader.TokenType == JsonToken.Null)
+      {
+        return null;
+      }
+
+      JObject obj = JObject.Load(reader);
+      var result = CreateInstance() as Object;
+
+      foreach (var pair in GetProperties())
+      {
+        Object value = obj[pair.Key].ToObject(
+          GetValueType(pair.Value),
+          serializer
+        );
+        SetValue(pair.Value, result, value);
+      }
+
+      return result;
+    }
+
+    public override void WriteJson(
+      JsonWriter writer,
+      object value,
+      JsonSerializer serializer
+    )
+    {
+      JObject obj = new JObject();
+
+      foreach (var pair in GetProperties())
+      {
+        var val = GetValue(pair.Value, value);
+        obj[pair.Key] = JToken.FromObject(val, serializer);
+      }
+
+      obj.WriteTo(writer);
+    }
+  }
+}
---
+++ src/Unity/JSON/Vector3Converter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace USM.Unity.Json
+{
+  public class Vector3Converter : PartialConverter<Vector3>
+  {
+    protected override string[] GetPropertyNames()
+    {
+      return new[] { "x", "y", "z" };
+    }
+  }
+}
